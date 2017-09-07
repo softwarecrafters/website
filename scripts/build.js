0d5b0b0b8aca835c1dbab68e4427a5bbdeca1795
Raimo Radczewski <raimoradczewski@googlemail.com>
2017-09-07 13:53:45 +0200

    Add build step so communities can add themselves
    Similar to how coderetreat.org handles events, this enables communities
    to just send us a pull request with their community information and
    opens this up to a crowdsourcing effort.
    
    NPM/Node is just there to merge (later also to validate) the JSON files
    so we can serve them from one place.
    
    Next steps could be:
    - Adding schema validation (requires us to come up with a schema)
    - Wiring Travis up so we can have tested PRs
    - Creating a `target/` folder from which the deployable artifacts are
    sourced so we don't publish `yarn.lock`, `package.json` and such.

---
+++ scripts/build.js
@@ -0,0 +1,14 @@
+const glob = require('glob');
+const path = require('path');
+const fs   = require('fs');
+
+const communityFiles = glob.sync(path.resolve(__dirname, '../communities/')+'/*.json');
+
+// validate
+
+// write to communities.json
+const communities = communityFiles.map(file => {
+  return JSON.parse(fs.readFileSync(file).toString());
+});
+
+fs.writeFileSync(path.resolve(__dirname, '../communities.json'), JSON.stringify(communities, undefined, 2));
