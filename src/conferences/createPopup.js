c88af347e9d773069b01d9694e3efb596932a52c
Raimo Radczewski <raimoradczewski@googlemail.com>
2018-02-25 22:17:23 +0100

    Show upcoming date and coc in conference popup

--- src/conferences/createPopup.js
+++ src/conferences/createPopup.js
@@ -1,12 +1,46 @@
+import jsJoda from 'js-joda';
+
+const createCocText = conference =>
+  conference.properties.coc
+    ? `<p><a href="${conference.properties.coc}">Code of Conduct</a></p>`
+    : '';
+
+const createDateText = conference => {
+  if (conference.properties.start == null) return '';
+
+  const {
+    start,
+    end,
+    singleDay,
+    isUpcoming,
+    daysUntil
+  } = conference.properties;
+
+  const dateFormatted =
+    (singleDay ? start : `${start} - ${end}`) +
+    (isUpcoming ? ` (in ${daysUntil} days)` : '');
+
+  if (!isUpcoming) {
+    return `<p><strong>Last:&nbsp;</strong>${dateFormatted}`;
+  }
+
+  return `<p><strong>Next:&nbsp;</strong>${dateFormatted}`;
+};
+
 export default conference => {
   const div = document.createElement('div');
   div.classList.add('popup');
   div.classList.add('conference-popup');
 
+  const dateLine = createDateText(conference);
+  const cocLine = createCocText(conference);
+
   div.innerHTML = `
     <h1><a target="_blank" href="${conference.properties.url}">${
     conference.properties.name
   }</a></h1>
+    ${dateLine}
+    ${cocLine}
   `;
 
   return div;
--- src/conferences/dataSource.js
+++ src/conferences/dataSource.js
@@ -1,21 +1,40 @@
-import conferences from "../../conferences.json";
+import conferences from '../../conferences.json';
+import jsJoda from 'js-joda';
+const { LocalDate, ChronoUnit } = jsJoda;
 
 const dataSource = {
-  type: "geojson",
+  type: 'geojson',
   cluster: true,
   data: {
-    type: "FeatureCollection",
-    features: conferences.map(conference => ({
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: conference.location.coordinates
-      },
-      properties: {
+    type: 'FeatureCollection',
+    features: conferences.map(conference => {
+      const props = {
         url: conference.url,
-        name: conference.name
+        name: conference.name,
+        coc: conference['code-of-conduct']
+      };
+
+      if (conference['next-date']) {
+        props.start = conference['next-date'].start;
+        props.end = conference['next-date'].end;
+        props.singleDay =
+          conference['next-date'].start === conference['next-date'].end;
+
+        const parsedStart = LocalDate.parse(props.start);
+        const parsedEnd = LocalDate.parse(props.end);
+        props.isUpcoming = parsedStart.isAfter(LocalDate.now());
+        props.daysUntil = LocalDate.now().until(parsedStart, ChronoUnit.DAYS);
       }
-    }))
+
+      return {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: conference.location.coordinates
+        },
+        properties: props
+      };
+    })
   }
 };
 
